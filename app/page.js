2c697b8e997ea246ce85d810de35b7a57cafb10d
mosespace <kisakyemoses50@gmail.com>
2023-07-25 18:50:03 +0300

    feat: initial modification

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
+import CategoryListing from "./components/CategoryListing";
 import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
     <main className='navbar-section'>
-      <Navbar/>
+      {/* HERO COMPONENT */}
+      <Hero/>
+      {/* CATEGORY  LISTING */}
+      <CategoryListing/>
     </main>
   );
 }
