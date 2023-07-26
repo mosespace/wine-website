692473418a827a0798f359fa7377892d2666d50f
mosespace <kisakyemoses50@gmail.com>
2023-07-26 09:45:36 +0300

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import CategoryListing from "./components/CategoryListing";
-import Navbar from "./components/Navbar";
+import Trending from "./components/Trending";
 export default function Home() {
   return (
     <main className='navbar-section'>
       {/* HERO COMPONENT */}
-      <Hero/>
+      <Hero />
       {/* CATEGORY  LISTING */}
-      <CategoryListing/>
+      <CategoryListing />
+      {/* Tranding Today */}
+      <Trending />
     </main>
   );
 }
