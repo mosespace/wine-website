7445b8fdede0a549f8ad288d02bc7aa3b73ebd55
mosespace <kisakyemoses50@gmail.com>
2023-07-26 16:04:44 +0300

    feat: inital commit

--- app/components/Trending.jsx
+++ app/components/Trending.jsx
@@ -213,6 +213,35 @@ export default function Trending() {
             </a>
           </div>
         </div>
+        <div className='card'>
+          <div className='upper-icons'>
+            <div className='span-class'>
+              <span className='percentage'>-5%</span>
+              <span>NEW</span>
+            </div>
+            <AiOutlineHeart size={25} />
+          </div>
+          <div className='image'>
+            <Image
+              src={Liquer}
+              alt='the alt tag goes here'
+              width={50}
+              height={50}
+            />
+          </div>
+          <h2>Wine</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, suscipit.
+          </p>
+          <div className='button-component'>
+            <h3>$3000</h3>
+            <a href='#'>
+              <AiOutlineShoppingCart />
+              Buy
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
