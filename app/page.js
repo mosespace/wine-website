00d53e833b109a239a08d3b90fd02dd107198070
mosespace <kisakyemoses50@gmail.com>
2023-07-26 16:04:49 +0300

    feat: inital commit

--- app/page.js
+++ app/page.js
@@ -2,13 +2,17 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import CategoryListing from "./components/CategoryListing";
 import Trending from "./components/Trending";
+import MobileNav from "./components/MobileNav";
 export default function Home() {
   return (
     <main className='navbar-section'>
       {/* HERO COMPONENT */}
-      <Hero />
+
+      {/* <Hero /> */}
+      <MobileNav />
       {/* CATEGORY  LISTING */}
       <CategoryListing />
+
       {/* Tranding Today */}
       <Trending />
     </main>
