502c663b777faa940a475ab5a8f3af3d5e60b393
mosespace <kisakyemoses50@gmail.com>
2023-07-27 09:33:36 +0300

    feat: inital modification!

--- app/page.js
+++ app/page.js
@@ -3,18 +3,29 @@ import Hero from "./components/Hero";
 import CategoryListing from "./components/CategoryListing";
 import Trending from "./components/Trending";
 import MobileNav from "./components/MobileNav";
+import Faq from "./components/Faq";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className='navbar-section'>
       {/* HERO COMPONENT */}
-
       {/* <Hero /> */}
       <MobileNav />
+
       {/* CATEGORY  LISTING */}
       <CategoryListing />
 
       {/* Tranding Today */}
-      <Trending />
+      {/* <Trending /> */}
+
+      {/* FAQs */}
+      <Faq />
+      {/* Call To Action */}
+      {/* <CallToAction /> */}
+
+      {/* FOoter Section */}
+      {/* <Footer /> */}
     </main>
   );
 }
