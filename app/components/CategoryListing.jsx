934d93f802ce0df61982ceb382dbb46eb4fe8948
mosespace <kisakyemoses50@gmail.com>
2023-07-27 09:33:56 +0300

    feat: inital modification!

--- app/components/CategoryListing.jsx
+++ app/components/CategoryListing.jsx
@@ -2,10 +2,6 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Liquer from "../../public/images/liquer.png";
-import Champagane from "../../public/images/Champagane.png";
-import Spirits from "../../public/images/Spirits.png";
-import Whiskey from "../../public/images/Whiskey.png";
-import Wine from "../../public/images/Wine.png";
 
 // Import Swiper styles
 import "swiper/css";
@@ -21,136 +17,86 @@ export default function CategoryListing() {
     <section className='category-listing'>
       <div className='real-section'>
         <Swiper
-          slidesPerView={5}
+          slidesPerView={1}
           spaceBetween={10}
           freeMode={true}
           pagination={{
             clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
           }}
           navigation={true}
           modules={[FreeMode, Pagination, Navigation]}
           className='categorySwiper'
         >
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Whiskey</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Whiskey</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Spirits</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Champegane & Sparkling</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Wine</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Liquer</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Whiskey</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />{" "}
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Whiskey</h2>
           </SwiperSlide>
           <SwiperSlide>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
+            <div className='slide-image'>
+              <Image src={Liquer} alt='the alt tag goes here' fill />
+            </div>
             <h2>Whiskey</h2>
           </SwiperSlide>
         </Swiper>
       </div>
     </section>
   );
-}
-
-{
-  /* <div className='category-card'>
-<div className='card-slider'>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-</div>
-{/* <div className='card-slider'>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-    possimus Hello.
-  </p>
-
-</div> */
 }
