034b1f7cdb3b7bf974e1586f6a22e8dcc408f494
mosespace <kisakyemoses50@gmail.com>
2023-07-29 10:33:22 +0300

    feat: used props

--- app/components/Trending.jsx
+++ app/components/Trending.jsx
@@ -3,245 +3,44 @@ import Liquer from "../../public/images/Whiskey.png";
 import Image from "next/image";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 
-export default function Trending() {
+export default function Trending({ title, bg, data }) {
   return (
-    <section className='trending-section'>
+    <section className='trending-section' style={{ background: bg }}>
       <div className='trending-title'>
-        <h1>Trending Today</h1>
+        <h1>{title}</h1>
       </div>
       <div className='trending-grid'>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
+        {data?.map((product, i) => {
+          return (
+            <div className='card' key={i}>
+              <div className='upper-icons'>
+                <div className='span-class'>
+                  <span className='percentage'>-5%</span>
+                  <span>NEW</span>
+                </div>
+                <AiOutlineHeart size={25} />
+              </div>
+              <div className='image'>
+                <Image
+                  src={product.Thumbnail}
+                  alt='the alt tag goes here'
+                  width={50}
+                  height={50}
+                />
+                
+              </div>
+              <h2>{product.title}</h2>
+              <p>{product.short_description}</p>
+              <div className='button-component'>
+                <h3>{product.CurrentPrice}</h3>
+                <a href='#'>
+                  <AiOutlineShoppingCart />
+                  Buy
+                </a>
+              </div>
             </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-icons'>
-            <div className='span-class'>
-              <span className='percentage'>-5%</span>
-              <span>NEW</span>
-            </div>
-            <AiOutlineHeart size={25} />
-          </div>
-          <div className='image'>
-            <Image
-              src={Liquer}
-              alt='the alt tag goes here'
-              width={50}
-              height={50}
-            />
-          </div>
-          <h2>Wine</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, suscipit.
-          </p>
-          <div className='button-component'>
-            <h3>$3000</h3>
-            <a href='#'>
-              <AiOutlineShoppingCart />
-              Buy
-            </a>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
