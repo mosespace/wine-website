112185a54613ce9385fa3558e2e4ea6b4f51e1b9
mosespace <kisakyemoses50@gmail.com>
2023-08-02 16:20:58 +0300

    feat: initial commit

--- app/categories/[slug]/page.js
+++ app/categories/[slug]/page.js
@@ -1,14 +1,28 @@
 import { getCategories } from "@/app/api/categories/route";
+import { getProducts } from "@/app/api/products/route";
 import DetailedCategories from "@/app/components/DetailedCategories";
+import Products from "@/app/components/Products";
+import SideBar from "@/app/components/SideBar";
 import React from "react";
 
 export default async function page({ params: { slug } }) {
   const categories = await getCategories();
 
   const category = categories?.find((category) => category.slug == slug);
+
+  const products = await getProducts();
+
+  const catId = category.id;
+  const catProducts = products.filter(
+    (product) => product.category_id == catId
+  );
+
   return (
-    <section className='detailed-section'>
-      <DetailedCategories data={category} />
+    <section className='container'>
+      <SideBar />
+      <div className='category_products'>
+        <Products data={catProducts} title='All PRODUCTS' />
+      </div>
     </section>
   );
 }
