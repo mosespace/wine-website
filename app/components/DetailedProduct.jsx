fc8c051b11436b4330bc84822a23639f71d7959e
mosespace <kisakyemoses50@gmail.com>
2023-08-03 19:03:19 +0300

    feat: initial commit

--- app/components/DetailedProduct.jsx
+++ app/components/DetailedProduct.jsx
@@ -1,15 +1,59 @@
 "use client";
-import React, { useRef, useState } from "react";
 import Image from "next/image";
 import { MdAddShoppingCart } from "react-icons/md";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 export default function DetailedProduct({ data }) {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
-    <section className='flex-container'>
+    <section className='flexs-container'>
       <div className='image-container'>
-        <div className='used-image'>
-          <Image src={data.image} alt={data.title} fill />
-        </div>
+        <Swiper
+          style={{
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          }}
+          spaceBetween={10}
+          navigation={false}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className='mySwiper2'
+        >
+          {data.images.map((image) => {
+            return (
+              <SwiperSlide>
+                <img src={image} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView={4}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className='mySwiper'
+        >
+          {data.images.map((image) => {
+            return (
+              <SwiperSlide>
+                <img src={image} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
       <div className='text-container'>
         <p>{data.keywords.splice(0, 2).join(", ")}💚</p>
