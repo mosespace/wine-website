eba9fa842ca72d96c13f881849fd39b0a585f251
mosespace <kisakyemoses50@gmail.com>
2023-08-03 11:53:21 +0300

    feat: initial commit

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "../styles/main.scss";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import MobileNav from "./components/MobileNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body className={inter.className}>
         <Navbar />
+        <MobileNav />
         {children}
         {/* FOoter Section */}
         <Footer />
