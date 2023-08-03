a59a2669795fc29c71920d9e2d4004731123f42d
mosespace <kisakyemoses50@gmail.com>
2023-08-03 09:28:20 +0300

    feat: initial modification

--- app/components/Burner.jsx
+++ app/components/Burner.jsx
@@ -1,28 +1,20 @@
 import React from "react";
-import Use from "../../public/images/Mixers & Soft Drinks.png";
 import Image from "next/image";
 
-export default function Burner() {
+export default function Burner({ data, bg='#fff'}) {
   return (
     <div className='burner'>
-      <div className='burner-details'>
+      <div className='burner-details' style={{background: bg}}>
         <div className='left-col'>
-          <h1>Mixers & Soft Drinks 🍷</h1>
-          <p>
-            Print custom business cards in a variety of high-quality paper
-            stocks, finishes, and sizes. Showcase your professional edge today.
-          </p>
+          <h1>{data.title} 🍷</h1>
+          <p>{data.description}</p>
           <ul>
-            <li>mixers</li>
-            <li>soft drinks</li>
-            <li>soda</li>
-            <li>tonic water</li>
+            <li>{data.keywords.splice(0, 4).join(" ")}</li>
           </ul>
         </div>
         <div className='right-col'>
-          {/* <h1>Hello World</h1> */}
           <div className='col-image'>
-            <Image src={Use} alt='Hello World' fill />
+            <Image src={data.image} alt={data.slug} fill />
           </div>
         </div>
       </div>
