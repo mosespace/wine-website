9db9ad880b1a020bf1b563e4225e810be7ee863f
mosespace <kisakyemoses50@gmail.com>
2023-08-12 15:40:44 +0300

    feat: configured getting data to the api

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -4,15 +4,44 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESND_API_KEY);
 
-export async function GET() {
+export async function POST(request) {
+  const requestData = await request.json();
+
+  const {
+    prodTitle,
+    productImage,
+    productPrice,
+    client_name,
+    vistor_name,
+    client_phone,
+    client_quantity,
+    client_pickupdate,
+  } = requestData;
+
+  const emailContent = (
+    <Email
+      data={{
+        prodTitle,
+        productImage,
+        productPrice,
+        client_name,
+        vistor_name,
+        client_phone,
+        client_quantity,
+        client_pickupdate,
+      }}
+    />
+  );
+
   resend.sendEmail({
-    from: "E&D-Wine<customer@jazzafricaadventures.com>",
+    // from: "E&D-Wine<customer@jazzafricaadventures.com>",
     to: "kisakyemoses50@gmail.com",
     subject: "New Order",
-    react: <Email />,
+    react: emailContent,
   });
 
   return NextResponse.json({
-    message: "Your Order Has Been Sent Sucessfully",
+    message: "Your Order Has Been Sent Successfully",
   });
 }
+
