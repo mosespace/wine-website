bcb254a0b3a58699e8d8ab417f24fe6c68c8a0d6
mosespace <kisakyemoses50@gmail.com>
2023-08-21 11:54:20 +0300

    feat: initial commit

--- app/context/context.jsx
+++ app/context/context.jsx
@@ -6,6 +6,7 @@ export const productContext = createContext();
 
 export function Context({ children }) {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
   const [cart, setCart] = useState([]);
 
@@ -15,17 +16,38 @@ export function Context({ children }) {
         const productsResponse = await fetch("/api/products");
         const productsData = await productsResponse.json();
         setProducts(productsData);
+        // setLoading(true);
 
         const categoriesResponse = await fetch("/api/categories");
         const categoriesData = await categoriesResponse.json();
         setCategories(categoriesData);
+
+        const storedCart = JSON.parse(localStorage.getItem("cartItems")) || [];
+        setCart(storedCart);
       } catch (error) {
         console.log("An error occurred while fetching data", error);
+        // setLoading(false);
+      } finally {
+        setLoading(false); // Set loading to false after data is fetched
       }
     }
 
     fetchData();
   }, []);
+
+  // Initialize cart items from local storage on first load
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem("cartItems")) || [];
+    setCart(storedCart);
+  }, []);
+
+  // Update local storage whenever cart items change
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cart));
+  }, [cart]);
+
+  const retreviedData = JSON.parse(localStorage.getItem("cartItems"));
+  // console.log(retreviedData);
 
   const addToCart = (product) => {
     setCart((prevCart) => [...prevCart, product]);
@@ -35,8 +57,14 @@ export function Context({ children }) {
     }
   };
 
+  const removeFromCart = (productID) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productID));
+  };
+
   return (
-    <productContext.Provider value={{ products, categories, cart, addToCart }}>
+    <productContext.Provider
+      value={{ products, loading, categories, cart, removeFromCart, addToCart }}
+    >
       {children}
     </productContext.Provider>
   );
